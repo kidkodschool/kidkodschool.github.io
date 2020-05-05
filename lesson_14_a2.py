8808a00baa84adf4fc7e254630d893fde5ea6001
unknown <like2pwnyou@yandex.ru>
2020-05-05 18:49:50 +0300

    lesson 14 added

---
+++ lesson_14_a2.py
@@ -0,0 +1,9 @@
+authrs = {
+    "Charles Dickens": "1870",
+    "William Thackeray": "1863",
+    "Anthony Trollope": "1882",
+    "Gerard Manley Hopkins": "1889"
+
+for author date in authors.items{}:
+    print "%s" % authors + " died in " + "%d." % Date
+}
