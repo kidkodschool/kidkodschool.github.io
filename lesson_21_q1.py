021f011095d8ec1f966482c7d7adb90d938f64e8
unknown <like2pwnyou@yandex.ru>
2020-11-25 15:06:17 +0300

    upd

---
+++ lesson_21_q1.py
@@ -0,0 +1,7 @@
+def get_average(marks):
+    return sum(marks) // len(marks)
+
+get_average([2, 2, 2, 2])
+get_average([1, 5, 87, 45, 8, 8])
+get_average([2,5,13,20,16,16,10])
+get_average([1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7])
---
+++ lesson_21_q2.py
@@ -0,0 +1,5 @@
+def flip(d, a):
+    return sorted(a) if d == 'R' else sorted(a, reverse=True)
+
+assert flip('L', [1, 4, 5, 3, 5]) == [5, 5, 4, 3, 1]
+assert flip('R', [3, 2, 1, 2]) == [1, 2, 2, 3]
