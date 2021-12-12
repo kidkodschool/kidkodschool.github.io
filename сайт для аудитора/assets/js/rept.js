ec695b71607cb8240ec0220a4e6ba31d56c2593e
raferalston <like2pwnyou@yandex.ru>
2021-12-12 20:00:55 +0300

    Add files via upload

---
+++ сайт для аудитора/assets/js/rept.js
@@ -0,0 +1,8 @@
+
+const navButton = document.querySelector('.menu-hamburger');
+
+navButton.addEventListener('click', navEvent);
+
+function navEvent(event) {
+    navButton.classList.toggle('open');
+}
